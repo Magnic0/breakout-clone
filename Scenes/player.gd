2977a4930f625671ccf0bd667bcfb802769f0c83
Nicolas Medeiros <nicolsoliverg@gmail.com>
2023-10-04 22:37:32 -0300

    start project, script player control

---
+++ Scenes/player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+
+func _physics_process(_delta):
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	move_and_slide()
